3a71f18bd6e2e0911da14afc485b8b7992772976
velpcode <velpmail@gmail.com>
2024-07-09 15:34:11 -0400

    Initial Commit

---
+++ presence/presenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  presenceApp.swift
+//  presence
+//
+//  Created by Velp on 2024-07-09.
+//
+
+import SwiftUI
+
+@main
+struct presenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
