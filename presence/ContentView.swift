cfd963eeda7d0190675e5c2332a32095d0150b8c
velpcode <velpmail@gmail.com>
2024-07-10 15:50:58 -0400

    Dude

--- presence/ContentView.swift
+++ presence/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            Text("PLACEHOLDER")
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Presence Tracker!")
+                Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
