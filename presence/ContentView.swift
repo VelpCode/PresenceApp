3628f77e3f16a0a6beddd0abfa3072ad34cd69c6
velpcode <velpmail@gmail.com>
2024-07-11 16:00:01 -0400

    weather app design

--- presence/ContentView.swift
+++ presence/ContentView.swift
@@ -14,9 +14,24 @@ struct ContentView: View {
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             VStack {
                 Text("London, ON")
-                    .font(.system(size: 32, weight: .medium, design: .default)).foregroundColor(.white)
-                Spacer()
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                
+                VStack(spacing: 4) {
                     
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                        
+                    Text("76°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                }
+                
+                Spacer()
             }
         }
     }
